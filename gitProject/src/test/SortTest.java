5db225b7829a15eff0a1a7253bbb418eacf78325
it <it@DESKTOP-KKAQ0RK>
2017-01-16 14:19:31 +0900

    commit

---
+++ gitProject/src/test/SortTest.java
@@ -0,0 +1,18 @@
+package test;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums={57,5,10,7,63,9};
+		
+		sort(nums,1);
+		sort(nums,2);
+
+	}
+	private static void sort(int[] nums,int orderby){
+		//占占占占占占ㅇㅅㅇ占占占占占占
+	}
+
+}
